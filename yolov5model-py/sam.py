55883f75157a095ce398bcbc57142bae12bdd456
Kim Lichtenberg <2727165@student.vu.nl>
2024-05-07 13:21:50 +0200

    Did a lot, forgot to push during the attempts. Restructured the structure of the github, brought the yolov5m model outside the venv to make sure we are training the right one, did a bunch of cleanup.

--- yolov5model-py/sam.py
+++
@@ -1,3 +0,0 @@
-from ultralytics.data.annotator import auto_annotate
-
-auto_annotate(data="C:\\Users\\fifag\\Documents\\kim-fifa\\crispy-couscous\\yolov5model-py\\part0", det_model="yolov8x.pt", sam_model='sam_b.pt', output_dir="\\Users\\fifag\\Documents\\kim-fifa\\crispy-couscous\\yolov5model-py\\part0\\frames")
